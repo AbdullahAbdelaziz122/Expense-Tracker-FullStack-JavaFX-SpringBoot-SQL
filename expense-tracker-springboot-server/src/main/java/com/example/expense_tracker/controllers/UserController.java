89be0d93214f7539494e10784fdfd4d5f1ebb9fe
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-10-16 21:37:09 +0300

    updated the login api to accept request body of LoginRequest DTO

--- expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/controllers/UserController.java
+++ expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.example.expense_tracker.controllers;
 
 
+import com.example.expense_tracker.DTO.LoginRequest;
 import com.example.expense_tracker.models.User;
 import com.example.expense_tracker.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -26,8 +29,11 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public ResponseEntity<User> loginUser(@RequestParam String email, @RequestParam String password){
-        User user = userService.login(email, password);
-        return ResponseEntity.ok(user);
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest request){
+        User user = userService.login(request.getEmail(), request.getPassword());
+        return ResponseEntity.ok(Map.of(
+                        "message", "login successful",
+                        "user", user
+        ));
     }
 }
