ec32b6a3d31f09f8384acf7613a99936f46ceca0
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-10-17 00:05:38 +0300

    Created the signUp Controller

---
+++ expense-tracker-client/src/main/java/org/example/Controllers/SignUpController.java
@@ -0,0 +1,16 @@
+package org.example.Controllers;
+
+import org.example.views.SignUpView;
+
+public class SignUpController {
+    SignUpView signUpView;
+
+    public SignUpController(SignUpView signUpView){
+        this.signUpView = signUpView;
+        initialize();
+    }
+
+    public void initialize(){
+        
+    }
+}
