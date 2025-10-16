50af5cafbdaa193b47bf8f033abdfd5736ef8953
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-10-16 16:08:04 +0300

    user data model added

---
+++ expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/models/User.java
@@ -0,0 +1,30 @@
+package com.example.expense_tracker.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="created_at")
+    private LocalDate created_at;
+}
