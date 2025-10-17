1c9ffbeb5dd8067e6acd14da86bccfb7c49960dc
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-10-17 11:43:30 +0300

    Finished the Register Method for user

--- expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/services/UserService.java
+++ expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.expense_tracker.services;
 
+import com.example.expense_tracker.exceptions.EmailAlreadyExist;
 import com.example.expense_tracker.exceptions.UserNotAuthorizedException;
 import com.example.expense_tracker.exceptions.UserNotFoundException;
 import com.example.expense_tracker.models.User;
@@ -7,6 +8,8 @@ import com.example.expense_tracker.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.logging.Logger;
 import org.springframework.stereotype.Service;
 
@@ -43,5 +46,29 @@ public class UserService {
 
         logger.info("User logged in successfully: " + email);
         return user;
+    }
+
+
+    public User registerUser(String name, String email, String password) {
+        if (checkEmailAlreadyExist(email)) {
+            logger.warning("Attempt to register user with existing email: "+ email);
+            throw new EmailAlreadyExist(email);
+        }
+
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        //todo : Hash the password before saving it to the database
+        user.setPassword(password);
+        user.setCreatedAt(LocalDate.now());
+
+        User savedUser = userRepository.save(user);
+        logger.info("New user registered successfully: "+ email);
+        return savedUser;
+    }
+
+
+    private boolean checkEmailAlreadyExist(String email){
+        return userRepository.findByEmail(email).isPresent();
     }
 }
