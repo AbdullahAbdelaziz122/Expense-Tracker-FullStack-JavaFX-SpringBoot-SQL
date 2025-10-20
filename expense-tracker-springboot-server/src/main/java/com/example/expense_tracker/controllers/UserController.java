cf069a9431d7100c013bcba98eb75ebfac757c8d
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-10-20 18:57:37 +0300

    Cleaned the response from the server with a fixed API Response for clarity

--- expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/controllers/UserController.java
+++ expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/controllers/UserController.java
@@ -1,8 +1,10 @@
 package com.example.expense_tracker.controllers;
 
 
+import com.example.expense_tracker.DTO.ApiResponse;
 import com.example.expense_tracker.DTO.LoginRequest;
 import com.example.expense_tracker.DTO.RegisterRequest;
+import com.example.expense_tracker.exceptions.UserNotFoundException;
 import com.example.expense_tracker.models.User;
 import com.example.expense_tracker.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -22,35 +24,46 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<User> getUserById(@PathVariable Long userId){
-        User user = userService.getUserById(userId);
-        return ResponseEntity.ok(user);
+    public ResponseEntity<ApiResponse<?>> getUserById(@PathVariable Long userId){
+        try{
+            User user = userService.getUserById(userId);
+            return ResponseEntity.ok(new ApiResponse<User>(true, "User Found", user));
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, ex.getMessage(), null));
+        }
     }
 
     @GetMapping()
     public ResponseEntity<?> getUserByEmail(@RequestParam String email){
-        User user = userService.getUserByEmail(email);
-        return ResponseEntity.ok(Map.of(
-                "message", "user found",
-                "user", user
-        ));
+        try {
+            User user = userService.getUserByEmail(email);
+            return ResponseEntity.ok(new ApiResponse<User>(true, "User Found", user));
+        }catch (Exception ex){
+            return ResponseEntity.ok(new ApiResponse<>(false, ex.getMessage(), null));
+        }
+
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest request){
-        User user = userService.login(request.email(), request.password());
-        return ResponseEntity.ok(Map.of(
-                        "message", "login successful",
-                        "user", user
-        ));
+
+        try {
+            User user = userService.login(request.email(), request.password());
+            return ResponseEntity.ok(new ApiResponse<User>(true, "Login Successful", user));
+        }catch (Exception ex){
+            return ResponseEntity.ok(new ApiResponse<>(false, ex.getMessage(), null));
+        }
+
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody RegisterRequest request){
-        User user = userService.registerUser(request.name(), request.email(), request.password());
-        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
-                "message", "user created",
-                "user", user
-        ));
+        try {
+
+            User user = userService.registerUser(request.name(), request.email(), request.password());
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<User>(true, "User Created", user));
+        }catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse<>(false, ex.getMessage(), null));
+        }
     }
 }
