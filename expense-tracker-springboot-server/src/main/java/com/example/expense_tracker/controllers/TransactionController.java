6aea9f7a258140699fa330c887126aa6669e2b80
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-10-28 10:01:40 +0300

    Added post Transaction endpoint

--- expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/controllers/TransactionController.java
+++ expense-tracker-springboot-server/src/main/java/com/example/expense_tracker/controllers/TransactionController.java
@@ -1,4 +1,49 @@
 package com.example.expense_tracker.controllers;
 
+import com.example.expense_tracker.DTO.ApiResponse;
+import com.example.expense_tracker.DTO.TransactionRequest;
+import com.example.expense_tracker.DTO.TransactionResponse;
+import com.example.expense_tracker.exceptions.TransactionCategoryNotFound;
+import com.example.expense_tracker.exceptions.UserNotFoundException;
+import com.example.expense_tracker.services.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/transaction")
 public class TransactionController {
+
+
+    private final TransactionService transactionService;
+
+    //post
+    @PostMapping()
+    public ResponseEntity<?> postTransaction(@RequestBody TransactionRequest request){
+
+        try {
+
+
+            TransactionResponse response = transactionService.createTransaction(request);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(
+                    new ApiResponse<TransactionResponse>(
+                            true,
+                            "Transaction Created Successfully",
+                            response
+                    )
+            );
+        }catch (UserNotFoundException ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, ex.getMessage(), null));
+        }catch (TransactionCategoryNotFound ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse<>(false, ex.getMessage(), null));
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse<>(false, ex.getMessage(), null));
+        }
+    }
 }
